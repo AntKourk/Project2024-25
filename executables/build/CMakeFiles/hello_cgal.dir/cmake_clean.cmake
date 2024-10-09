58729975789a928f580fae5e0e8f7c13510ef9ef
AntKourk <antonikourkou@yahoo.com>
2024-10-09 20:22:50 +0300

    creation of a cgal program and compilation tect

---
+++ executables/build/CMakeFiles/hello_cgal.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cgal.dir/hello_cgal.cpp.o"
+  "CMakeFiles/hello_cgal.dir/hello_cgal.cpp.o.d"
+  "hello_cgal"
+  "hello_cgal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cgal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ executables/hello_cgal.cpp
@@ -0,0 +1,12 @@
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/point_generators_2.h>
+#include <iostream>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+// typedef Kernel::Point_2 ,Point_2;
+
+int main() {
+    Kernel::Point_2 p(2.0, 2.0); // Δημιουργούμε ένα σημείο (1.0, 1.0)
+    std::cout << "Hello CGAL! Το σημείο p είναι: " << p << std::endl;
+    return 0;
+}
